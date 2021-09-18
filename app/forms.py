710cb94522a43004ad2a4c9128fa4c0142c69f9f
michael otieno <michael.otieno@student.moringaschool.com>
2021-09-19 01:51:33 +0300

    add registration form and log in

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,BooleanField,SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[Required()])
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    verify_password = PasswordField('Repeat Password', validators=[Required(), EqualTo('password')])
+    submit = SubmitField('Sign In')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
